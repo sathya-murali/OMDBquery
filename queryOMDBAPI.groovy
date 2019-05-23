b589bf1c1f041b34f262a688790ac6b3f498ffd7
SathyaM <techsathya77@gmail.com>
2019-05-23 10:11:40 -0500

    SM - Fixed inputstream space and read

--- queryOMDBAPI.groovy
+++ queryOMDBAPI.groovy
@@ -4,24 +4,40 @@ Purpose : This file queries OMDB API based on Movie Name
 */
 // http://www.omdbapi.com/?t=terminator&apikey=f4cba32c
 
-def queryomdb() {
+
+def queryomdb(String whichMovie = movieName) {
+
     def baseURL = 'http://www.omdbapi.com/'
     def apikey = 'f4cba32c'
-    println "Please input Movie Name to Query"
-    String movieName = System.in.newReader().readLine().trim()
-    //movieName = movieName.toString()
-    println "Running Query for " + movieName
-    apiOut = new ByteArrayOutputStream()
-    apiErr = new ByteArrayOutputStream()
+    def apiOut = new ByteArrayOutputStream()
+    def apiErr = new ByteArrayOutputStream()
+
     run = """
-    curl '${baseURL}\\?t=${movieName}&apikey=${apikey}'
+    curl '${baseURL}\\?t=${whichMovie}&apikey=${apikey}'
     """  
     runProc = ['bash', '-c',  run].execute()
     runProc.waitForProcessOutput(apiOut, apiErr)
+        if (apiOut =~ "Movie not found!") {
+            println "Movie Not Found, Please check the Name you provided :) :)"
+            
+        }  
+        else  {
+            println "STDOUT " + apiOut
+            println "STDERR " + apiErr
+        }  
     
-    println "STDOUT " + apiOut
-    println "STDERR " + apiErr
+    
     //println run.split(',')
 }
 
-queryomdb()
+def processinput() {
+
+    println "Please input Movie Name to Query"
+    def movieName = System.in.newReader().readLine().trim()
+    //movieName = movieName.toString()
+    println "Running Query for " + movieName
+    movieName = movieName.replaceAll("\\s", '+')
+
+    queryomdb(movieName)
+}
+processinput()
