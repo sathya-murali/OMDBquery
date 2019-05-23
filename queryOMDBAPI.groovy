1780bb7d6361c4e901404e32d23bf6b4a6b79b36
SathyaM <techsathya77@gmail.com>
2019-05-23 11:27:39 -0500

    SM - Updating with JSon query insted of Shell

--- queryOMDBAPI.groovy
+++ queryOMDBAPI.groovy
@@ -2,39 +2,35 @@
 Author : Sathyanarayanan
 Purpose : This file queries OMDB API based on Movie Name
 */
-// http://www.omdbapi.com/?t=terminator&apikey=f4cba32c
 
+import groovy.json.JsonSlurper;
+import groovy.json.JsonOutput;
+import groovy.json.JsonBuilder;
 
 def queryomdb(String whichMovie = movieName) {
 
-    def baseURL = 'http://www.omdbapi.com/'
     def apikey = 'f4cba32c'
     def apiOut = new ByteArrayOutputStream()
     def apiErr = new ByteArrayOutputStream()
 
-    run = """
-    curl '${baseURL}\\?t=${whichMovie}&apikey=${apikey}'
-    """  
-    runProc = ['bash', '-c',  run].execute()
-    runProc.waitForProcessOutput(apiOut, apiErr)
-        if (apiOut =~ "Movie not found!") {
+    URL urlBuild = new URL ("http://www.omdbapi.com/?t=${whichMovie}&apikey=${apikey}")
+    def movieApiSlurper = new JsonSlurper().parse(urlBuild)
+    String movieApiOut = new JsonBuilder(movieApiSlurper).toPrettyString()
+
+        if (movieApiOut =~ "Movie not found!") {
             println "Movie Not Found, Please check the Name you provided :) :)"
             
         }  
         else  {
-            println "STDOUT " + apiOut
-            println "STDERR " + apiErr
+            println "STDOUT " + movieApiOut
         }  
-    
-    
-    //println run.split(',')
+
 }
 
 def processinput() {
 
     println "Please input Movie Name to Query"
     def movieName = System.in.newReader().readLine().trim()
-    //movieName = movieName.toString()
     println "Running Query for " + movieName
     movieName = movieName.replaceAll("\\s", '+')
 
