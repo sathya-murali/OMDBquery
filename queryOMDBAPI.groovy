13a8eb2a512c7c18d4e6811a74ff28b9d0d563b2
SathyaM <techsathya77@gmail.com>
2019-05-23 09:37:32 -0500

    SM - Updating process method for query

--- queryOMDBAPI.groovy
+++ queryOMDBAPI.groovy
@@ -8,14 +8,20 @@ def queryomdb() {
     def baseURL = 'http://www.omdbapi.com/'
     def apikey = 'f4cba32c'
     println "Please input Movie Name to Query"
-    def movieName = System.in.newReader().readLine()
-
-    println "Running Query"
+    String movieName = System.in.newReader().readLine().trim()
+    //movieName = movieName.toString()
+    println "Running Query for " + movieName
+    apiOut = new ByteArrayOutputStream()
+    apiErr = new ByteArrayOutputStream()
     run = """
-    curl "${baseURL}\\?t=${movieName}&apikey=${apikey}"
+    curl '${baseURL}\\?t=${movieName}&apikey=${apikey}'
     """  
-    run = run.execute().text
-    println run.split(',')
+    runProc = ['bash', '-c',  run].execute()
+    runProc.waitForProcessOutput(apiOut, apiErr)
+    
+    println "STDOUT " + apiOut
+    println "STDERR " + apiErr
+    //println run.split(',')
 }
 
 queryomdb()
