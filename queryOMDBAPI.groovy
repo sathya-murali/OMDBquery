75f7f3d57157d416a666b26e93ad36b0587c480a
SathyaM <techsathya77@gmail.com>
2019-05-23 23:55:33 -0500

    SM - Adding key encryption

--- queryOMDBAPI.groovy
+++ queryOMDBAPI.groovy
@@ -1,6 +1,7 @@
 /*
 Author : Sathyanarayanan
 Purpose : This file queries OMDB API based on Movie Name
+Version: 3
 */
 
 import groovy.json.JsonSlurper;
@@ -9,10 +10,42 @@ import groovy.json.JsonBuilder;
 
 def queryomdb(String whichMovie = movieName) {
 
-    def apikey = 'f4cba32c'
+    
     def apiOut = new ByteArrayOutputStream()
     def apiErr = new ByteArrayOutputStream()
+    //StrongTextEncryptor textEncryptor = new StrongTextEncryptor();
+    //textEncryptor.setPassword('MYPASS-');
+    //def apikey = textEncryptor.decrypt('jejq9eMsFoi2Z3K1MUvor+K1kYTBKWND')
+    def dcry
+    def pth
+    runPreDecrypt = """
+        wget -q -O "/usr/src/jasypt-1.9.2-dist.zip" "https://sourceforge.net/projects/jasypt/files/jasypt/jasypt%201.9.2/jasypt-1.9.2-dist.zip" 
+        cd /usr/src/
+        unzip -qq "/usr/src/jasypt-1.9.2-dist.zip"
+        chmod 500 "/usr/src/jasypt-1.9.2/bin/decrypt.sh"
+    """
 
+    run = ['bash', '-c', runPreDecrypt].execute()
+    run.waitForProcessOutput(apiOut, apiErr)
+        apiOut = apiOut.size()
+        apiErr = apiErr.size()
+
+        if ( ! apiOut.equals(0) ) {
+            println "STDOUT " + apiOut
+        }
+        
+            if ( ! apiOut.equals(0) ) {
+            println "STDOUT " + apiErr
+        }
+
+    def decryptOut = new ByteArrayOutputStream()
+    def decryptErr = new ByteArrayOutputStream()
+
+     runDecrypt = """
+     /usr/src/jasypt-1.9.2/bin/decrypt.sh input=jejq9eMsFoi2Z3K1MUvor+K1kYTBKWND password=MYPASS- verbose=false
+    """
+    
+    def apikey = runDecrypt.execute().text
     URL urlBuild = new URL ("http://www.omdbapi.com/?t=${whichMovie}&apikey=${apikey}")
     def movieApiSlurper = new JsonSlurper().parse(urlBuild)
     String movieApiOut = new JsonBuilder(movieApiSlurper).toPrettyString()
@@ -22,7 +55,7 @@ def queryomdb(String whichMovie = movieName) {
             
         }  
         else  {
-            println "STDOUT " + movieApiOut
+            println movieApiOut
         }  
 
 }
